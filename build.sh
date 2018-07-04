265d98fca6e03aff82405faaf65bf10a25f2f16a
levozavr <30433199+levozavr@users.noreply.github.com>
2018-07-04 16:09:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ BUILD_TYPE==-DCMAKE_BUILD_TYPE=Debug
 CC=gcc
 CXX=g++
 
+mkdir build
+cd build
+
 cmake -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE $EXTRA_CMAKE_FLAGS -DBUILD_TESTING=ON .. || exit 1
 cmake --build . -- -v || exit 1
 ASAN_OPTIONS=alloc_dealloc_mismatch=0 ctest . || exit 1
